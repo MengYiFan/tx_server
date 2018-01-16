46d6bc97538d3dd9716a0be218645a2673032efa
mz_zml <mz_zml@163.com>
2018-01-16 23:04:17 +0800

    Upload config files

---
+++ config/development.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: 'development',
+  port: 3000,
+  mysql: {
+    host: 'localhost',
+    port: '3306',
+    user: 'mish',
+    password: '11',
+    database: 'tx',
+    charset: 'utf8_general_ci'
+  },
+  redis: {
+    host: '127.0.0.1',
+    port: '6379',
+    password: 'REDIS'
+  }
+}
